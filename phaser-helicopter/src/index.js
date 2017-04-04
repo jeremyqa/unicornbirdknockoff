e153b2b4ef04cc08b5cf78558180165b3bbcd222
Jeremy Block <jblock@cbinsights.com>
2017-04-04 18:41:52 -0400

    i think this resolution restriction is better. prevents performance from getting weird when you fullscreen on a big display

--- phaser-helicopter/src/index.js
+++ phaser-helicopter/src/index.js
@@ -6,18 +6,21 @@ import GameOver from 'states/GameOver';
 
 class Game extends Phaser.Game {
 
-	constructor() {
+    constructor() {
+        let maxWidth = 1600;
+        let maxHeight = 900;
+        let windowWidth = window.innerWidth * window.devicePixelRatio;
+        let windowHeight = window.innerHeight * window.devicePixelRatio;
 
-		super(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO);
+        super(Math.min(maxWidth, windowWidth), Math.min(maxHeight, windowHeight), Phaser.AUTO);
+        this.state.add('Boot', Boot, false);
+        this.state.add('Preload', Preload, false);
+        this.state.add('GameTitle', GameTitle, false);
+        this.state.add('Main', Main, false);
+        this.state.add('GameOver', GameOver, false);
 
-		this.state.add('Boot', Boot, false);
-		this.state.add('Preload', Preload, false);
-		this.state.add('GameTitle', GameTitle, false);
-		this.state.add('Main', Main, false);
-		this.state.add('GameOver', GameOver, false);
-
-		this.state.start('Boot');
-	}
+        this.state.start('Boot');
+    }
 
 }
 
