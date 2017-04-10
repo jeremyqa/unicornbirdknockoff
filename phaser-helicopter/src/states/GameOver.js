ee84a0ec784f10c4fa044bf518e01c3975b42fc5
Jeremy Block <jblock@cbinsights.com>
2017-04-10 16:02:22 -0400

    add coin to game over screen and make it a link to the jobs page i think there's still some weirdness when you open the game in a weirdly sized window

--- phaser-helicopter/src/states/GameOver.js
+++ phaser-helicopter/src/states/GameOver.js
@@ -12,7 +12,7 @@ class GameOver extends Phaser.State {
         this.sprite.inputEnabled = true;
         this.sprite.events.onInputDown.add(this.restartGame, this);
 
-        this.text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, `YOU'RE OUT OF MONEY :(\nClick the unicorn to try again.`, {
+        this.text = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, `YOU'RE OUT OF MONEY :(\nClick the unicorn to try again.`, {
             font: "72px Arial",
             fill: "#ABCDEF",
             align: "center"
@@ -21,19 +21,30 @@ class GameOver extends Phaser.State {
     }
 
     showHiringMessage() {
-        let hiringText = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 300, `Built during CB Insights Hack Day (We're hiring)`, {
+        let hiringText = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 200, `Built during CB Insights Hack Day (We're hiring)\nClick the coin to see job openings:`, {
             font: "20px Arial",
             fill: "#ABCDEF",
             align: "center"
         });
         hiringText.anchor.setTo(0.5, 0.5);
         hiringText.inputEnabled = true;
-        hiringText.events.onInputDown.add(function() { window.open("https://www.cbinsights.com/jobs", "_blank");}, this);
+        hiringText.events.onInputDown.add(this.openJobsPage, this);
+
+        this.coinSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 300, 'coin');
+        this.coinSprite.anchor.setTo(0.5, 0.5);
+        this.coinSprite.animations.add('spin', [0,1,2,3,4,5,6,7,8,10], 10, true);
+        this.coinSprite.animations.play('spin', 10, true);
+        this.coinSprite.inputEnabled = true;
+        this.coinSprite.events.onInputDown.add(this.openJobsPage, this);
 
     }
 
+    openJobsPage() {
+        window.open("https://www.cbinsights.com/jobs", "_blank");
+    }
+    
     showHighPandaCost() {
-        let pandaText = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 200, `Most expensive panda ever bought: $${localStorage.getItem("highPandaCost")*-1}`, {
+        let pandaText = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 100, `Most expensive panda ever bought: $${localStorage.getItem("highPandaCost")*-1}`, {
             font: "20px Arial",
             fill: "#ABCDEF",
             align: "center"
